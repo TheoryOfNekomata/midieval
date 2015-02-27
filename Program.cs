e65167305f55bf79cfb3ea43ac0d3b586122a05b
Allan Crisostomo <kiiroifuriku@hotmail.com>
2015-02-27 13:26:48 +0800

    Restructure code

--- Program.cs
+++ Program.cs
@@ -19,7 +19,38 @@ namespace MidiEval {
 
 			foreach(var _track in _sequence) {
 				for(var i = 0; i < _track.Count; i++) {
-					System.Windows.Forms.MessageBox.Show("#" + i + ": " + _track.GetMidiEvent(i).DeltaTicks);
+					var _midiEvent = _track.GetMidiEvent(i);
+
+					var _messageType = _midiEvent.MidiMessage.MessageType;
+					var _status = _midiEvent.MidiMessage.Status;
+					var _bytes = _midiEvent.MidiMessage.GetBytes();
+					
+					if(_messageType == MessageType.Channel) {
+						var _event = _status & 0xF0;
+						var _channel = _status & 0x0F;
+						var _key = _bytes[1] & 0x7F;
+						var _velocity = _bytes[2] & 0x7F;
+
+						var _eventName = "";
+
+						switch(_event) {
+							case 0x80:
+								_eventName = "Note OFF";
+								break;
+							case 0x90:
+								_eventName = "Note ON";
+								break;
+							default:
+								_eventName = "Others";
+								continue;
+						}
+
+						System.Windows.Forms.MessageBox.Show(
+							"Ch. " + _channel + " " + _eventName + " @" + _midiEvent.AbsoluteTicks + ":" +
+							" Key #" + _key +
+							" (" + _velocity + ")"
+						);
+					}
 				}
 			}
 			

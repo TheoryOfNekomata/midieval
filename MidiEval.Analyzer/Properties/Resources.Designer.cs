811b5c745c459a613ea4d1b88f22cc1b7e894ba3
Allan Crisostomo <kiiroifuriku@hotmail.com>
2015-03-05 18:49:42 +0800

    Update assemblies

--- MidiEval.Analyzer/Properties/Resources.Designer.cs
+++ MidiEval.Analyzer/Properties/Resources.Designer.cs
@@ -59,35 +59,5 @@ namespace MidiEval.Analyzer.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _1 {
-            get {
-                object obj = ResourceManager.GetObject("1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _2 {
-            get {
-                object obj = ResourceManager.GetObject("2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap MidiEval_Mockup {
-            get {
-                object obj = ResourceManager.GetObject("MidiEval Mockup", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
